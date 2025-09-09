554c09f5692bbfdf6c61e4effde0498b01116b75
Tejaswini Penneni <tejaswinipenneni@gmail.com>
2025-09-09 14:47:40 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'  // or HashRouter
 import './index.css'
+import { HashRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* Option 1: BrowserRouter with basename */}
-    <BrowserRouter basename="/github-portfolio">
-      <App />
-    </BrowserRouter>
-
-    {/* OR Option 2: HashRouter (if you want to avoid refresh 404s) */}
-    {/*
     <HashRouter>
       <App />
     </HashRouter>
-    */}
   </React.StrictMode>
+
 )
