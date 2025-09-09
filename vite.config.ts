2be1560964ff7f457a97d5ee6d16feea9832f0c6
Tejaswini Penneni <tejaswinipenneni@gmail.com>
2025-09-09 14:46:37 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   optimizeDeps: {
-//     exclude: ['lucide-react'],
-//   },
-// });
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/github-portfolio/',   // 👈 matches your URL path
+  base: '/github-portfolio/',   // <-- MUST match your site path
 })
